0ff328a918a3a30a6446ec3befee849239ac36bf
queerxdisasster <7germania7@gmail.com>
2025-02-05 22:32:10 +0300

    123

---
+++ src/main/java/com/queerxdisasster/websocketnotifications/WebsocketNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.queerxdisasster.websocketnotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketNotificationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketNotificationsApplication.class, args);
+    }
+
+}
